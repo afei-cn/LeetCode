a84ba3bec503bd50b426aae2a5a9144a04af9aec
afei <815609532@qq.com>
2018-11-05 22:13:10 +0800

    add README.md

--- 303. Range Sum Query - Immutable/src/NumArray.java
+++ 303. Range Sum Query - Immutable/src/NumArray.java
@@ -5,6 +5,32 @@
  * @date: 2018年11月5日
  */
 
-public class NumArray {
+class NumArray {
+    
+    private int[] sums;
 
+    public NumArray(int[] nums) {
+        sums = new int[nums.length];
+        if (nums.length == 0) {
+            return;
+        }
+        sums[0] = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            sums[i] += sums[i - 1] + nums[i];
+        }
+    }
+    
+    public int sumRange(int i, int j) {
+        if (i == 0) {
+            return sums[j];
+        } else {
+            return sums[j] - sums[i - 1];
+        }
+    }
 }
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray obj = new NumArray(nums);
+ * int param_1 = obj.sumRange(i,j);
+ */
