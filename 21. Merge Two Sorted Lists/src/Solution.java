2c0ece316024b20edb65cd3cb0cbc6775c113532
afei <815609532@qq.com>
2018-11-17 22:42:14 +0800

    add project: 21. Merge Two Sorted Lists

---
+++ 21. Merge Two Sorted Lists/src/Solution.java
@@ -0,0 +1,68 @@
+/**
+ * Copyright © 2018 by afei. All rights reserved.
+ * 
+ * @author: afei
+ * @date: 2018年11月17日
+ */
+
+public class Solution {
+    
+    public static void main(String[] args) {
+        ListNode l1 = new ListNode(1);
+        l1.next = new ListNode(2);
+        l1.next.next = new ListNode(4);
+        ListNode l2 = new ListNode(1);
+        l2.next = new ListNode(3);
+        l2.next.next = new ListNode(4);
+        
+        ListNode merge = mergeTwoLists(l1, l2);
+        while (merge != null) {
+            System.out.print(merge.val + "->");
+            merge = merge.next;
+        }
+    }
+
+    public static ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null) {
+            return l2;
+        }
+        if (l2 == null) {
+            return l1;
+        }
+        ListNode head = null;
+        if (l1.val < l2.val) {
+            head = l1;
+            l1 = l1.next;
+        } else {
+            head = l2;
+            l2 = l2.next;
+        }
+        ListNode tail = head;
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                tail.next = l1;
+                tail = l1;
+                l1 = l1.next;
+            } else {
+                tail.next = l2;
+                tail = l2;
+                l2 = l2.next;
+            }
+        }
+        if (l1 == null) {
+            tail.next = l2;
+        } else {
+            tail.next = l1;
+        }
+        return head;
+    }
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+}
