1850a4c67084531a31bc415041d83d372c11cd47
afei <815609532@qq.com>
2018-12-17 22:54:55 +0800

    optimize code

--- 567. Permutation in String/src/Solution.java
+++ 567. Permutation in String/src/Solution.java
@@ -18,29 +18,26 @@ class Solution {
         }
         int[] count1 = new int[26]; // s1每个字符出现的次数
         int[] count2 = new int[26]; // s2每个字符出现的次数
+        // 1. 进行统计
         for (int i = 0; i < s1.length(); i++) {
             count1[s1.charAt(i) - 'a']++;
             count2[s2.charAt(i) - 'a']++;
         }
-
-        int[] diff = new int[26]; // s1和s2每个字符的数量差距
-        for (int i = 0; i < diff.length; i++) {
-            diff[i] = count2[i] - count1[i];
-        }
-
+        // 2. 滑动窗口，滑块长度始终为 s1.length()
         for (int i = s1.length(); i < s2.length(); i++) {
-            if (isSame(diff)) {
+            if (isSame(count1, count2)) {
                 return true;
             }
-            diff[s2.charAt(i - s1.length()) - 'a']--; // 去掉首个字符
-            diff[s2.charAt(i) - 'a']++; // 添加最新的字符
+            count2[s2.charAt(i - s1.length()) - 'a']--; // 去掉滑块当前的首个字符
+            count2[s2.charAt(i) - 'a']++; // 添加最新的字符到滑块中
         }
-        return isSame(diff);
+        return isSame(count1, count2);
     }
 
-    public static boolean isSame(int[] diff) {
-        for (int i = 0; i < diff.length; i++) {
-            if (diff[i] != 0) {
+    // 有且仅当 count1 中所有值都等于 count2 中对应值时满足条件
+    public static boolean isSame(int[] count1, int[] count2) {
+        for (int i = 0; i < count1.length; i++) {
+            if (count1[i] != count2[i]) {
                 return false;
             }
         }
